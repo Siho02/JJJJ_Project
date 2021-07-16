76e78cef974f8be3c1ac5565ef1706b546222118
dlwlsdudo1 <83809838+dlwlsdudo1@users.noreply.github.com>
2021-07-16 17:20:58 +0900

    1

--- Project/App.py
+++ Project/App.py
@@ -1,6 +1,10 @@
 from flask.json import jsonify
 from flask import Flask, render_template, request,url_for
 
+=======
+from flask import Flask, render_template, request,url_for
+from flask import current_app as current_app
+>>>>>>> Stashed changes
 
 app = Flask(__name__)
 
